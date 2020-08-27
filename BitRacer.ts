e67bc94a7b43329c91cc1ea716843484c5023e29
Jer-Wei Rau <Jer-Wei@users.noreply.github.com>
2020-08-28 01:05:23 +0800

    Initial files for MakeCode project

---
+++ BitRacer.ts
@@ -0,0 +1,149 @@
+//
+const N76_ADDR = 0x10
+//% weight=0 color=#f98020 icon="\uf1ba" block="BitRacer"
+namespace BitRacer {
+    export enum Motors {
+        //% blockId="left motor" block="left"
+        M_R = 0,
+        //% blockId="right motor" block="right"
+        M_L = 1,
+        //% blockId="all motor" block="all"
+        All = 2
+    }
+    export enum IR_Sensors {
+        //% blockId="IR1_Sensors" block="IR1"
+        IR1 = 0x03,
+        //% blockId="IR2_Sensors" block="IR2"
+        IR2 = 0x04,
+        //% blockId="IR3_Sensors" block="IR3"
+        IR3 = 0x05,
+        //% blockId="IR4_Sensors" block="IR4"
+        IR4 = 0x06,
+        //% blockId="IR5_Sensors" block="IR5"
+        IR5 = 0x07
+    }	
+	export enum LineColor {
+        //% blockId="White" block="White"
+        White = 0x0A,
+        //% blockId="Black" block="Black"
+        Black = 0x0B
+    }
+	export enum LEDs {
+        //% blockId="LED_right" block="right"
+        LED_R = 8,
+        //% blockId="LED_left" block="left"
+        LED_L = 16
+    }
+	export enum LEDswitch {
+        //% blockId="on" block="on"
+        on = 0,
+        //% blockId="off" block="off"
+        off = 1
+    }
+    
+	//% weight=100
+    //% blockId=motor_MotorRun block="motor|%index|PWM value|%PWM"
+    //% PWM.min=-1000 PWM.max=1000
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=3
+    export function motorRun(index: Motors, PWM: number): void {
+        let i2cbuf = pins.createBuffer(3);
+        if (index == 0) {
+            i2cbuf[0] = 0x02;
+            i2cbuf[1] = PWM >> 8;
+            i2cbuf[2] = PWM;
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
+        }
+        if (index == 1) {
+            i2cbuf[0] = 0x00;
+            i2cbuf[1] = PWM >> 8;
+            i2cbuf[2] = PWM;
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
+        }
+        if (index == 2) {	
+            i2cbuf[0] = 0x00;
+            i2cbuf[1] = PWM >> 8;
+            i2cbuf[2] = PWM;
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
+            i2cbuf[0] = 0x02;
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
+        }
+    }
+	
+	//% weight=90
+    //% blockId=sensor_readIR block="read |%SensorID sensor"
+    //% SensorID.fieldEditor="gridpicker" SensorID.fieldOptions.columns=3
+    export function readIR(SensorID: IR_Sensors): number {
+        pins.i2cWriteNumber(
+            N76_ADDR,
+            SensorID,
+            NumberFormat.UInt8LE,
+            false
+        )
+        return pins.i2cReadNumber(N76_ADDR, NumberFormat.UInt16BE, false)
+    }
+	//% weight=85
+	//% blockId=sensor_readIR2 block="read IRsensor |%SensorID"
+	//% SensorIDs.min=0 SensorIDs.max=4
+    export function readIR2(SensorIDs: number): number {
+        pins.i2cWriteNumber(
+            N76_ADDR,
+            SensorIDs+3,
+            NumberFormat.UInt8LE,
+            false
+        )
+        return pins.i2cReadNumber(N76_ADDR, NumberFormat.UInt16BE, false)
+    }
+	//% weight=80
+    //% blockId=LED_Set block="LED|%LedPin|%status"
+    //% LedPin.fieldEditor="gridpicker" LedPin.fieldOptions.columns=1
+    //% status.fieldEditor="gridpicker" status.fieldOptions.columns=1
+	export function LED(LedPin: LEDs,status: LEDswitch): void {
+        if (LedPin == LEDs.LED_R)
+		{
+            pins.digitalWritePin(DigitalPin.P8, status)
+		}
+        else if (LedPin == LEDs.LED_L)
+		{
+            pins.digitalWritePin(DigitalPin.P16, status)
+		}
+    }
+	
+	
+	//% color=#2080ff
+	//% weight=30
+	//% blockId=sensor_StartSampling block="Calibrate Begin"
+    export function CalibrateBegin(): void {
+        pins.i2cWriteNumber(
+            N76_ADDR,
+            0x09,
+            NumberFormat.UInt8LE,
+            false
+        )
+    }
+	
+	//% color=#2080ff
+	//% weight=20
+    //% blockId=sensor_EndSampling block="Calibrate End|%Color (Line)"
+    //% Color.fieldEditor="gridpicker" Color.fieldOptions.columns=1
+	export function CalibrateEnd(Color: LineColor): void {
+        pins.i2cWriteNumber(
+            N76_ADDR,
+            Color,
+            NumberFormat.UInt8LE,
+            false
+        )
+    }
+	
+	//% color=#2080ff
+    //% weight=10
+    //% blockId=sensor_Line block="read Line position"
+    export function readLine(): number {
+        pins.i2cWriteNumber(
+            N76_ADDR,
+            0x08,
+            NumberFormat.UInt8LE,
+            false
+        )
+        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Int16BE, false) / 1000
+    }
+}
---
+++ README.md
@@ -0,0 +1,45 @@
+# tools
+
+這是專為BitRacer測試用的micro:bit積木
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+
+
+> Open this page at [https://jer-wei.github.io/new-bitracer_block/](https://jer-wei.github.io/new-bitracer_block/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/jer-wei/new-bitracer_block** and import
+
+## Edit this project ![Build status badge](https://github.com/jer-wei/new-bitracer_block/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/jer-wei/new-bitracer_block** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/jer-wei/new-bitracer_block/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
